eb4c703f3c80c58b9869af94da0e3810cc3e876d
李磊 <lilei@bmos.net.cn>
2025-03-12 16:22:47 +0800

    fix: 路由

--- apps/docs/.vitepress/config/zh.ts
+++ apps/docs/.vitepress/config/zh.ts
@@ -73,6 +73,7 @@ export const zh = defineConfig({
             { text: 'iframe 微前端问题', link: '/blog/iframe' },
             { text: '删除 node_modules', link: '/blog/quickDeleteNodeModules' },
             { text: 'vue3 组件封装技巧', link: '/blog/componentSkill' },
+            { text: 'uni-app 自动更新', link: '/blog/autoUpdate' },
           ],
         },
       ],
